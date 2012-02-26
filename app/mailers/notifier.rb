a6753319fef7302b5811f15140ee155881fe7d52
Andy Litzinger <andy.litzinger@gmail.com>
2012-02-25 20:59:10 -0800

    fixed feature by putting code mail output on single lines

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -3,8 +3,6 @@ class Notifier < ActionMailer::Base
   def comment_updated(comment, user)
     @comment = comment
     @user = user
-    mail(to: user.email,
-         subject: "[ticketee] #{comment.ticket.project.name} -
-         #{comment.ticket.title}")
+    mail(to: user.email, subject: "[ticketee] #{comment.ticket.project.name} - #{comment.ticket.title}")
   end
 end
