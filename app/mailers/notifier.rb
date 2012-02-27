edb77b71c3a93e91e6a56b76e27b0a0da42d2261
Andy Litzinger <andy.litzinger@gmail.com>
2012-02-26 16:19:43 -0800

    add receiver class to receive emails

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -3,6 +3,7 @@ class Notifier < ActionMailer::Base
   def comment_updated(comment, user)
     @comment = comment
     @user = user
-    mail(to: user.email, subject: "[ticketee] #{comment.ticket.project.name} - #{comment.ticket.title}")
+    mail(to: user.email, subject: "[ticketee] #{comment.ticket.project.name} - #{comment.ticket.title}",
+        from: "Ticketee <ticketee+#{comment.project.id}+#{comment.ticket_id}@gmail.com>")
   end
 end
