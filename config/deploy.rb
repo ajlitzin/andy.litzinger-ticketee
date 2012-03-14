beab9749f2f659ae9a4f4512f9319a4bc5ffbdb0
Andy Litzinger <andy.litzinger@gmail.com>
2012-03-13 18:32:48 -0700

    ch 14 completed as by the rails3 book with nginx

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,18 @@
-require 'bundler/capistrano'
 $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
-set :rvm_type, :user
 require 'rvm/capistrano'
+require 'bundler/capistrano'
+
+set :rvm_type, :system
 
 set :application, "ticketee"
 set :repository,  "git@github.com:ajlitzin/andy.litzinger-ticketee.git"
 
+set :branch, "ch14-by-the-book"
+
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-set :port, 2222
+set :port, 2200
 
 role :web, "localhost"
 role :app, "localhost"
@@ -18,7 +21,9 @@ role :db,  "localhost", :primary => true # This is where Rails migrations will r
 
 set :user, "ticketeeapp.com"
 set :deploy_to, "/home/ticketeeapp.com/apps/#{application}"
+
 set :use_sudo, false
+
 set :keep_releases, 5
 
 # if you're still using the script/reaper helper you will need
@@ -33,9 +38,9 @@ set :keep_releases, 5
    end
  end
 
-# task :symlink_database_yml do
-#   run "rm #{release_path}/config/database.yml"
-#   run "ln -sfn #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-# end
+ task :symlink_database_yml do
+   run "rm #{release_path}/config/database.yml"
+   run "ln -sfn #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+ end
 # before "deploy:assets:precompile", "bundle:install"
-# after "bundle:install", "symlink_database_yml"
+ after "bundle:install", "symlink_database_yml"
